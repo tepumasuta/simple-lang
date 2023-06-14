2d4a11f8cf660ed368520af5fb42cfe5a25695c2
tepumasuta <thinkingocean2@gmail.com>
2023-06-14 04:38:15 +0300

    Added `Interpreter` with heap, stack and program as members and `Interpret` as method & defined heap and stack sizes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 #include <string>
+#include <array>
+
+
+constexpr size_t ce_HeapSize = 1 << 14;
+constexpr size_t ce_StackSize = 1 << 10;
+
+
+class Interpreter
+{
+private:
+    std::array<uint8_t, ce_HeapSize> m_Heap;
+    std::array<uint8_t, ce_StackSize> m_Stack;
+    const std::string m_Program;
+public:
+    Interpreter(const std::string& program) : m_Program(program) {}
+    ~Interpreter() {}
+
+    void Interpret()
+    {
+        std::cout << "Hello from interpreter!\n"
+                  << "The program received: \n`" << m_Program << '`' << std::endl;
+    }
+};
+
 
 using namespace std::string_literals;
 
@@ -7,6 +31,9 @@ const std::string program = ""s;
 
 
 int main() {
-    std::cout << "The program is:\n`" << program << '`' << std::endl;
+    Interpreter interpreter(program);
+
+    interpreter.Interpret();
+
     return 0;
 }
